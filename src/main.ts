443321f88dd0eac04ea7e6e6608b6edcbcf086d2
Andy <115222358+AndyMznc@users.noreply.github.com>
2023-10-13 22:07:55 +0200

    Logique pour gagner un round
    Création d'une fonction pour gagner un round, avec des console.log()

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,58 @@ const playerChoiceValue: string = prompt(
   'Choose an action from "rock", "paper", "scissors".'
 ) as string;
 
-function playerSelection(playerSelection: string, computerSelection: string) {
-  if (actions.includes(playerSelection.toLowerCase())) {
-    console.log("ok, good");
-  } else {
-    console.log("wrong value");
+let playerScore: number = 0;
+let computerScore: number = 0;
+
+function playRound(playerSelection: string, computerSelection: string) {
+  console.log(`Player: ${playerSelection}, Computer: ${computerSelection}`);
+
+  // Égalité
+  if (playerSelection === computerSelection) {
+    console.log("égalité");
+    return;
+  }
+
+  switch (playerSelection) {
+    case "rock":
+      if (computerSelection === "paper") {
+        console.log("perdu");
+      } else if (computerSelection === "scissors") {
+        console.log("gagné");
+      }
+      break;
+
+    case "paper":
+      if (computerSelection === "scissors") {
+        console.log("perdu");
+      } else if (computerSelection === "rock") {
+        console.log("gagné");
+      }
+      break;
+
+    case "scissors":
+      if (computerSelection === "rock") {
+        console.log("perdu");
+      } else if (computerSelection === "paper") {
+        console.log("gagné");
+      }
+      break;
+
+    default:
+      console.log("Choix invalide");
+      break;
   }
 }
-playerSelection(playerChoiceValue, computerChoiceValue);
+
+playRound(playerChoiceValue, computerChoiceValue);
+
+// function playerSelection(playerSelection: string, computerSelection: string) {
+//   let playerChoice = playerSelection.toLowerCase()
+
+//   if (!actions.includes(playerChoice)) {
+//     console.log("Please, select a valid value.");
+//   } else {
+
+//   }
+// }
+// playerSelection(playerChoiceValue, computerChoiceValue);
