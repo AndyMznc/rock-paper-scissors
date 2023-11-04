c143583df685867962af9f6f79b60a1880592073
Andy <115222358+AndyMznc@users.noreply.github.com>
2023-11-04 23:57:32 +0100

    Ajout de l'inferface utilisateur
    Réorganisation du projet en supprimant le choix de son action (prompt) par Event
    Nous avons aussi depuis l'interface les scores, le gagnant du round et du match

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,68 @@
-export const actions: string[] = ["rock", "paper", "scissors"];
-
+const buttons = document.querySelectorAll<HTMLButtonElement>(".btn");
+const scores = document.querySelector<HTMLParagraphElement>(".results__scores");
+const actions: string[] = ["rock", "paper", "scissors"];
+let lastActionsPlayed = document.querySelector<HTMLParagraphElement>(
+  ".results--last-actions"
+);
+let roundResult = document.querySelector<HTMLParagraphElement>(
+  ".results--round-result"
+);
 let playerScore: number = 0;
 let computerScore: number = 0;
+
+if (scores) {
+  scores.textContent = `PLAYER : ${playerScore} - ${computerScore} : COMPUTER`;
+}
 
 function getComputerChoice(): string {
   return actions[Math.floor(Math.random() * actions.length)];
 }
 
-function playRound(playerSelection: string, computerSelection: string) {
-  console.log(`Player: ${playerSelection}, Computer: ${computerSelection}`);
+function capitalizeFirstLetter(text: string): string {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
 
-  // Égalité
+function playRound(playerSelection: string, computerSelection: string) {
+  const cPlayerSelection = capitalizeFirstLetter(playerSelection);
+  const cComputerSelection = capitalizeFirstLetter(computerSelection);
+
+  if (lastActionsPlayed) {
+    lastActionsPlayed.textContent = `You play: ${cPlayerSelection} vs ${cComputerSelection}`;
+  }
+
   if (playerSelection === computerSelection) {
-    console.log("égalité");
+    if (roundResult) roundResult.textContent = "It's a tie this round";
     return;
   }
 
   switch (playerSelection) {
     case "rock":
       if (computerSelection === "paper") {
-        return computerScore++;
-      } else if (computerSelection === "scissors") {
-        return playerScore++;
+        if (roundResult) roundResult.textContent = "You lost this round";
+        computerScore++;
+      } else {
+        if (roundResult) roundResult.textContent = "You win this round";
+        playerScore++;
       }
       break;
 
     case "paper":
       if (computerSelection === "scissors") {
-        return computerScore++;
-      } else if (computerSelection === "rock") {
-        return playerScore++;
+        if (roundResult) roundResult.textContent = "You lost this round";
+        computerScore++;
+      } else {
+        if (roundResult) roundResult.textContent = "You win this round";
+        playerScore++;
       }
       break;
 
     case "scissors":
       if (computerSelection === "rock") {
-        return computerScore++;
-      } else if (computerSelection === "paper") {
-        return playerScore++;
+        if (roundResult) roundResult.textContent = "You lost this round";
+        computerScore++;
+      } else {
+        if (roundResult) roundResult.textContent = "You win this round";
+        playerScore++;
       }
       break;
 
@@ -45,33 +70,41 @@ function playRound(playerSelection: string, computerSelection: string) {
       console.log("Choix invalide");
       break;
   }
+
+  if (scores) {
+    scores.textContent = `PLAYER : ${playerScore} - ${computerScore} : COMPUTER`;
+  }
+  if (playerScore === 3) {
+    if (roundResult) roundResult.textContent = "You won the game!";
+  } else if (computerScore === 3) {
+    if (roundResult) roundResult.textContent = "Computer won the game!";
+  }
+}
+
+function handleButtonClick(this: HTMLButtonElement) {
+  const playerChoice = this.textContent?.toLowerCase() || "";
+  const computerChoice = getComputerChoice();
+  playRound(playerChoice, computerChoice);
+
+  if (playerScore === 3) {
+    if (roundResult) roundResult.textContent = "You won the game!";
+    removeEventListeners();
+  } else if (computerScore === 3) {
+    if (roundResult) roundResult.textContent = "Computer won the game!";
+    removeEventListeners();
+  }
+}
+
+function removeEventListeners() {
+  Array.from(buttons).forEach((button) => {
+    button.removeEventListener("click", handleButtonClick);
+  });
 }
 
 function game() {
-  while (playerScore < 3 && computerScore < 3) {
-    for (let i = 0; i < 5; i++) {
-      const playerChoiceValue: string = prompt(
-        'Choose an action from "rock", "paper", "scissors".'
-      ) as string;
-
-      const computerChoiceValue: string = getComputerChoice();
-
-      playRound(playerChoiceValue.toLowerCase(), computerChoiceValue);
-
-      console.log(`player : ${playerScore} - computer : ${computerScore}`);
-
-      if (playerScore >= 3 || computerScore >= 3) {
-        break;
-      }
-    }
-
-    if (playerScore >= 3 || computerScore >= 3) {
-      break;
-    }
-  }
-
-  playerScore == 3
-    ? console.log("You win !")
-    : console.log("You loose, maybe next time !");
+  Array.from(buttons).forEach((button) => {
+    button.addEventListener("click", handleButtonClick);
+  });
 }
+
 game();
